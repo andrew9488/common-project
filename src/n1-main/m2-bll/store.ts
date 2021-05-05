280a6cbb38decf09f24e09d3a0cfb4c8d26a16f9
andrew9488 <pashkevichandrew@gmail.com>
2021-05-05 19:31:56 +0300

    feat: create reducer for registration. Write action creator and thunk for api request

--- src/n1-main/m2-bll/store.ts
+++ src/n1-main/m2-bll/store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import {profileReducer} from "../m1-ui/profile/profile-reducer";
 import {authReducer} from "../../n2-features/f1-auth/a1-login/auth-reducer";
+import {registerReducer} from "../../n2-features/f1-auth/a2-registration/register-reducer";
 
 const reducer = combineReducers({
     profile: profileReducer,
-    auth: authReducer
+    auth: authReducer,
+    register: registerReducer
 
 })
 
---
+++ src/n2-features/f1-auth/a2-registration/register-reducer.ts
@@ -0,0 +1,39 @@
+import {Dispatch} from "redux";
+import {API} from "../../../n1-main/m3-dal/api";
+
+type ActionsType = ReturnType<typeof setRegistrationDataAC>
+
+type RegisterType = {
+    email: string
+    password: string
+}
+
+const initialState = {} as RegisterType
+
+type InitialStateType = typeof initialState
+
+export const registerReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case "REGISTER/SET-REGISTRATION-DATA":
+            return {
+                ...state,
+                email: action.email,
+                password: action.password
+            }
+        default:
+            return state
+    }
+}
+
+const setRegistrationDataAC = (email: string, password: string) =>
+    ({type: "REGISTER/SET-REGISTRATION-DATA", email, password} as const)
+
+const registerTC = (email: string, password: string) => (dispatch: Dispatch) => {
+    API.register(email, password)
+        .then(() => {
+            dispatch(setRegistrationDataAC(email, password))
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
