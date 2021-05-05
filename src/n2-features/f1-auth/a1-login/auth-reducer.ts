b1feadaef1410e708846ae1a8b683ce335954b16
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-05 21:36:36 +0300

    feat: add bll for logging in

--- src/n2-features/f1-auth/a1-login/auth-reducer.ts
+++ src/n2-features/f1-auth/a1-login/auth-reducer.ts
@@ -1,12 +1,53 @@
-type ActionsType = any
+import {Dispatch} from 'redux';
+import {API} from '../../../n1-main/m3-dal/api';
 
-const initialState = {}
+//types
+type ActionsType = ReturnType<typeof setLoginData>
 
-type InitialStateType = typeof initialState
+type UserDataType = {
+    email: string | null,
+    _id: string | null,
+    avatar: string | null,
+    name: string | null,
+    publicCardPacksCount: number
+}
+
+type InitialStateType = { userData: UserDataType }
+
+
+const initialState = {
+    userData: {
+        email: null,
+        _id: null,
+        avatar: null,
+        name: null,
+        publicCardPacksCount: 0,
+    }
+}
+
 
 export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
+        case 'SET-USER-DATA': {
+            return {...state, ...action.data}
+        }
         default:
             return state
     }
+}
+
+//actions
+export const setLoginData = (data: UserDataType) => ({type: 'SET-USER-DATA', data} as const)
+
+//thunks
+export const loginTC = (email: string, password: string, rememberMe: boolean) => (dispatch: Dispatch<ActionsType>) => {
+    API.login(email, password, rememberMe)
+        .then(response => {
+            const {email, _id, avatar, name, publicCardPacksCount} = response;
+            dispatch(setLoginData({email, _id, avatar, name, publicCardPacksCount}))
+        })
+        .catch(e => {
+            const error = e.response ? e.response.data.error : (e.message + ', more details in the console')
+            console.log(error);
+        })
 }
