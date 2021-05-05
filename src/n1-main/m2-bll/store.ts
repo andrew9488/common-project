9bc8a1236d07cdcb4152041c11a17dd97339fb19
andrew9488 <pashkevichandrew@gmail.com>
2021-05-05 22:31:50 +0300

    feat: creat recovery-reducer

--- src/n1-main/m2-bll/store.ts
+++ src/n1-main/m2-bll/store.ts
@@ -3,11 +3,13 @@ import {profileReducer} from "../m1-ui/profile/profile-reducer";
 import {authReducer} from "../../n2-features/f1-auth/a1-login/auth-reducer";
 import {registerReducer} from "../../n2-features/f1-auth/a2-registration/register-reducer";
 import thunk from "redux-thunk";
+import {recoveryReducer} from "../../n2-features/f1-auth/a3-recovery-password/recovery-reducer";
 
 const reducer = combineReducers({
     profile: profileReducer,
     auth: authReducer,
-    register: registerReducer
+    register: registerReducer,
+    forgot: recoveryReducer
 
 })
 
