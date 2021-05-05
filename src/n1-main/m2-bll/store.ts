ff14df64f0eb79e0b0312742c36d3b1045d379bb
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-05 22:49:36 +0300

    feat: add thunk middleware

--- src/n1-main/m2-bll/store.ts
+++ src/n1-main/m2-bll/store.ts
@@ -1,13 +1,15 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {profileReducer} from "../m1-ui/profile/profile-reducer";
 import {authReducer} from "../../n2-features/f1-auth/a1-login/auth-reducer";
 
+import thunk from "redux-thunk";
+
 const reducer = combineReducers({
     profile: profileReducer,
-    auth: authReducer
+    auth: authReducer,
 
 })
 
-export const store = createStore(reducer)
+export const store = createStore(reducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof reducer>
