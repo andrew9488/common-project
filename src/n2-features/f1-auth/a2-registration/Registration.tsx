253fa62f3241dd38905ad407d028e70c4cb46178
andrew9488 <pashkevichandrew@gmail.com>
2021-05-05 19:45:44 +0300

    feat: create page layout for Registration page. Add local state for inputs. Add onChangeHandler for it

--- src/n2-features/f1-auth/a2-registration/Registration.tsx
+++ src/n2-features/f1-auth/a2-registration/Registration.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 
-const Registration: React.FC = ()=>{
-    return(
+const Registration: React.FC = () => {
+
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+
+    const onChangePasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
+    return (
         <div>
-            Registration
+            <form>
+                <div>
+                    <span>Email: </span><input type="text" value={email} onChange={onChangeEmailHandler}/>
+                </div>
+                <div>
+                    <span>password: </span><input type="password" value={password} onChange={onChangePasswordHandler}/>
+                </div>
+                <div>
+                    <button type="submit">Register</button>
+                </div>
+            </form>
         </div>
     )
 }
