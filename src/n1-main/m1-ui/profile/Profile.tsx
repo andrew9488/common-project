4d12d358162e0253415192b5701f9fc4a7965038
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-07 14:36:10 +0300

    feat: add logout button and redirect to login is not authorized

--- src/n1-main/m1-ui/profile/Profile.tsx
+++ src/n1-main/m1-ui/profile/Profile.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from '../../m2-bll/store';
+import {logOutTC} from './profile-reducer';
+import {Redirect} from 'react-router-dom';
+import {PATH} from '../routes/Routes';
 
-const Profile: React.FC = ()=>{
-    return(
+const Profile: React.FC = () => {
+
+    const dispatch = useDispatch();
+
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
+
+    const onLogOutHandler = () => {
+        dispatch(logOutTC());
+    }
+
+    if (!isLoggedIn) {
+        return <Redirect to={PATH.LOGIN}/>
+    }
+
+    return (
         <div>
             Profile
+            <br/>
+            <button onClick={onLogOutHandler}>Log out</button>
         </div>
     )
 }
