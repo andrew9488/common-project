11a236e99c8872b86838a06110983d212b2fab0e
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-10 14:23:12 +0300

    fix: fix loading

--- src/n2-features/f1-auth/a1-login/auth-reducer.ts
+++ src/n2-features/f1-auth/a1-login/auth-reducer.ts
@@ -1,6 +1,6 @@
 import {API} from '../../../n1-main/m3-dal/api';
-import {setAppStatusAC} from "../../../n1-main/m1-ui/app-reducer";
-import {AppThunkType} from "../../../n1-main/m2-bll/store";
+import {setAppStatusAC} from '../../../n1-main/m1-ui/app-reducer';
+import {AppThunkType} from '../../../n1-main/m2-bll/store';
 
 //types
 export type AuthReducerActionType = ReturnType<typeof setLoginData>
@@ -54,25 +54,29 @@ export const setLoginError = (loginError: string) => ({type: 'auth/SET-LOGIN-ERR
 
 //thunks
 export const loginTC = (email: string, password: string, rememberMe: boolean): AppThunkType => dispatch => {
-    dispatch(setAppStatusAC("loading"))
+    dispatch(setAppStatusAC('loading'))
     API.login(email, password, rememberMe)
         .then(response => {
             const {email, _id, avatar, name, publicCardPacksCount} = response;
             dispatch(setLoginData({email, _id, avatar, name, publicCardPacksCount}));
             dispatch(setIsLoggedIn(true));
-            dispatch(setAppStatusAC("succeeded"))
+            dispatch(setAppStatusAC('succeeded'))
         })
         .catch(e => {
             const error = e.response ? e.response.data.error : (e.message + ', more details in the console')
             dispatch(setLoginError(error));
+            dispatch(setAppStatusAC('failed'));
         })
 }
 
 export const logOutTC = (): AppThunkType => dispatch => {
-    dispatch(setAppStatusAC("loading"))
+    dispatch(setAppStatusAC('loading'))
     API.logout()
         .then(response => {
             dispatch(setIsLoggedIn(false));
-            dispatch(setAppStatusAC("succeeded"))
+            dispatch(setAppStatusAC('succeeded'))
+        })
+        .catch(error => {
+            dispatch(setAppStatusAC('failed'));
         })
 }
