341088309074d2b31c78cc7208f984e32de7a5a6
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-11 15:15:09 +0300

    feat: add standard SuperDoubleRange component

---
+++ src/n1-main/m1-ui/common/SuperDoubleRange/SuperDoubleRange.tsx
@@ -0,0 +1,33 @@
+import React, {ChangeEvent} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+type SuperDoubleRangePropsType = {
+    onChangeRange: (value: [number, number]) => void
+    value: number | number[]
+}
+
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (props) => {
+    // сделать самому, можно подключать библиотеки
+
+    const handleChange = (event: ChangeEvent<{}>, newValue: any) => {
+        props.onChangeRange([newValue[0], newValue[1]])
+    };
+
+    return (
+        <div>
+            <Typography id="range-slider2" gutterBottom>
+            </Typography>
+            <Slider
+                value={props.value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider2"
+            />
+        </div>
+    );
+}
+
+
+
+export default SuperDoubleRange;
