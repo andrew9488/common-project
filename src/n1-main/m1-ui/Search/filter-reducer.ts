7b6317e8e1f6a4535a0969e2cbcd38c862caddbc
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-11 15:15:49 +0300

    feat: add reducer for filtration

---
+++ src/n1-main/m1-ui/Search/filter-reducer.ts
@@ -0,0 +1,39 @@
+import {AppThunkType} from '../../m2-bll/store';
+
+type ActionsType = ReturnType<typeof setSearchValueAC> | ReturnType<typeof setMinMaxValuesAC>
+
+const initialState = {
+    search: '',
+    min: 3,
+    max: 9,
+}
+
+type InitialStateType = typeof initialState
+
+export const filterReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'SET-SEARCH-VALUE': {
+            return {...state, search: action.value}
+        }
+        case 'SET-MIN-MAX-VALUES': {
+            return {...state, min: action.values[0], max: action.values[1]}
+        }
+        default:
+            return state
+    }
+}
+
+//actions
+
+export const setSearchValueAC = (value: string) => ({type: 'SET-SEARCH-VALUE', value} as const)
+export const setMinMaxValuesAC = (values: number[]) => ({type: 'SET-MIN-MAX-VALUES', values} as const)
+
+//thunks
+
+export const getPacksWithFilters = (): AppThunkType => (dispatch, getState) => {
+    let search = getState().filter.search;
+    let min = getState().filter.min;
+    let max = getState().filter.max;
+    //@ts-ignore
+    fetchPacksTC()
+}
