9f34e3b80a2b07892250749b96cac9077154f0e1
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-12 14:27:21 +0300

    feat: add thunks for pagination, portion pages changing and sorting by cards count

--- src/n1-main/m1-ui/Search/filter-reducer.ts
+++ src/n1-main/m1-ui/Search/filter-reducer.ts
@@ -1,13 +1,13 @@
 import {AppThunkType} from '../../m2-bll/store';
+import {fetchPacksTC, setPagesCountAC, setPageValueAC} from '../packs/packs-reducer';
+import {QueryPacksType} from '../../m3-dal/api';
 
 type ActionsType = ReturnType<typeof setSearchValueAC> | ReturnType<typeof setMinMaxValuesAC>
 
 const initialState = {
     search: '',
     min: 3,
-    max: 9,
-    page: 1,
-    pageCount: 4
+    max: 9
 }
 
 type InitialStateType = typeof initialState
@@ -30,16 +30,28 @@ export const filterReducer = (state: InitialStateType = initialState, action: Ac
 export const setSearchValueAC = (value: string) => ({type: 'SET-SEARCH-VALUE', value} as const)
 export const setMinMaxValuesAC = (values: number[]) => ({type: 'SET-MIN-MAX-VALUES', values} as const)
 
+
 //thunks
 
 export const getPacksWithFilters = (): AppThunkType => (dispatch, getState) => {
     let search = getState().filter.search;
     let min = getState().filter.min;
     let max = getState().filter.max;
-    //@ts-ignore
-    fetchPacksTC()
+    const searchParams = {packName: search, min, max} as Partial<QueryPacksType>
+    dispatch(fetchPacksTC(searchParams))
 }
 
-export const onPacksPageClickTC = (): AppThunkType => (dispatch) => {
+export const onPacksPageClickTC = (newPage: number): AppThunkType => (dispatch) => {
+    dispatch(setPageValueAC(newPage));
+    const params = {page: newPage}
+    dispatch(fetchPacksTC(params))
+}
 
+export const onPortionPacksChangeTC = (pagesCount: number): AppThunkType => (dispatch) => {
+    dispatch(setPagesCountAC(pagesCount))
+    dispatch(fetchPacksTC({pageCount: pagesCount}))
+}
+
+export const sortPackTC = (sortPacks: string): AppThunkType => (dispatch) => {
+    dispatch(fetchPacksTC({sortPacks}))
 }
