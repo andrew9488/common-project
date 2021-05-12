2a742f108ff6f78c28081ab06a0641803a44c614
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-12 14:28:53 +0300

    feat: add action creators for changing pagesCount and page

--- src/n1-main/m1-ui/packs/packs-reducer.ts
+++ src/n1-main/m1-ui/packs/packs-reducer.ts
@@ -1,36 +1,47 @@
-import {packsAPI, PackType, ResponsePackType} from "../../m3-dal/api"
-import {AppThunkType} from "../../m2-bll/store";
-import {setAppStatusAC, setIsInitializedAC} from "../app-reducer";
+import {packsAPI, PackType, QueryPacksType, ResponsePackType} from '../../m3-dal/api'
+import {AppThunkType} from '../../m2-bll/store';
+import {setAppStatusAC, setIsInitializedAC} from '../app-reducer';
 
 export type PacksReducerActionType = ReturnType<typeof setPacksDataAC>
+    | ReturnType<typeof setPageValueAC>
+    | ReturnType<typeof setPagesCountAC>
 
 const initialState = {} as ResponsePackType
 type InitialStateType = typeof initialState
 
 export const packsReducer = (state: InitialStateType = initialState, action: PacksReducerActionType): InitialStateType => {
     switch (action.type) {
-        case "PACKS/SET-PACKS":
+        case 'PACKS/SET-PACKS':
             return {
                 ...state, ...action.packsData
             }
+        case 'SET-CURRENT-PAGE-VALUE': {
+            return {...state, page: action.value}
+        }
+        case 'SET-CURRENT-PAGES-COUNT': {
+            return {...state, pageCount: action.value}
+        }
         default:
             return state
     }
 }
 
 export const setPacksDataAC = (packsData: ResponsePackType) =>
-    ({type: "PACKS/SET-PACKS", packsData} as const)
+    ({type: 'PACKS/SET-PACKS', packsData} as const)
 
-export const fetchPacksTC = (): AppThunkType => dispatch => {
-    dispatch(setAppStatusAC("loading"))
-    packsAPI.fetchPacks({})
+export const setPageValueAC = (value: number) => ({type: 'SET-CURRENT-PAGE-VALUE', value} as const)
+export const setPagesCountAC = (value: number) => ({type: 'SET-CURRENT-PAGES-COUNT', value} as const)
+
+export const fetchPacksTC = (queryObj?: Partial<QueryPacksType>): AppThunkType => dispatch => {
+    dispatch(setAppStatusAC('loading'))
+    packsAPI.fetchPacks(queryObj || {})
         .then(response => {
             dispatch(setPacksDataAC(response))
-            dispatch(setAppStatusAC("succeeded"))
+            dispatch(setAppStatusAC('succeeded'))
         })
         .catch(error => {
             console.log(error)
             dispatch(setIsInitializedAC(true))
-            dispatch(setAppStatusAC("failed"))
+            dispatch(setAppStatusAC('failed'))
         })
 }
