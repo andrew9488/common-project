d35a17f00a0f5c542aa4b6a0faba23822d0214c1
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-19 20:51:04 +0300

    chore: add plug for modals

--- src/n1-main/m1-ui/profile/Profile.tsx
+++ src/n1-main/m1-ui/profile/Profile.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../m2-bll/store';
 import {Redirect} from 'react-router-dom';
 import {PATH} from '../routes/Routes';
 import {RequestStatusType} from '../app-reducer';
 import {logOutTC} from '../../../n2-features/f1-auth/a1-login/auth-reducer';
+import Modal from '../../../n2-features/f2-modals/modal/Modal';
 
 
 const Profile: React.FC = () => {
@@ -17,6 +18,11 @@ const Profile: React.FC = () => {
         dispatch(logOutTC());
     }
 
+
+    //logic for delete pack with modals
+    const [showModal, setShowModal] = useState<boolean>(true);
+
+
     if (!isLoggedIn) {
         return <Redirect to={PATH.LOGIN}/>
     }
@@ -24,6 +30,27 @@ const Profile: React.FC = () => {
     return (
         <div>
             Profile
+            {/*Перенести*/}
+            <button>Delete card</button>
+            {/*{showModal && <Modal childrenHeight={220}*/}
+            {/*                     childrenWidth={400}*/}
+            {/*                     onDeleteClick={() => {*/}
+            {/*                     }}*/}
+            {/*                     onModalClose={() => setShowModal(false)}*/}
+            {/*                     type={'info'}*/}
+            {/*                     header={'Delete pack'}*/}
+            {/*                     buttonTitle={'Delete'}*/}
+            {/*                     packName={'Pack name'}/>}*/}
+
+            {showModal && <Modal childrenHeight={233}
+                                 childrenWidth={400}
+                                 onSaveClick={() => {
+                                 }}
+                                 onModalClose={() => setShowModal(false)}
+                                 type={'input'}
+                                 header={'Add new pack'}
+                                 buttonTitle={'Save'}
+                                 inputTitle={'Name pack'}/>}
             <br/>
             <button onClick={onLogOutHandler} disabled={appStatus === 'loading'}>Log out</button>
         </div>
