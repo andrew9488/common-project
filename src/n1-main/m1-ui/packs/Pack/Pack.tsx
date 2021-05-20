beda084f84a70c0853ba28e9988d1063d6f7516f
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-20 22:08:28 +0300

    feat: add Modals for delete/updates packs

--- src/n1-main/m1-ui/packs/Pack/Pack.tsx
+++ src/n1-main/m1-ui/packs/Pack/Pack.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import {NavLink} from "react-router-dom";
-import {PATH} from "../../routes/Routes";
+import React, {useState} from 'react';
+import {NavLink} from 'react-router-dom';
+import {PATH} from '../../routes/Routes';
 import {PackType} from '../../../m3-dal/packAPI';
+import Modal from '../../../../n2-features/f2-modals/modal/Modal';
 
 type PackPropsType = {
     pack: PackType
@@ -11,6 +12,8 @@ type PackPropsType = {
 
 export const Pack: React.FC<PackPropsType> = (props) => {
 
+    const [showDelModal, setShowDelModal] = useState<boolean>(false);
+    const [showEditModal, setShowEditModal] = useState<boolean>(false);
 
     return (
         <>
@@ -20,8 +23,30 @@ export const Pack: React.FC<PackPropsType> = (props) => {
                 <td>{props.pack.updated}</td>
                 <td>{props.pack.user_name}</td>
                 <td>
-                    <button onClick={() => props.deleteCardsPack(props.pack._id)}>del</button>
-                    <button onClick={() => props.updateCardsPackName(props.pack._id)}>update</button>
+                    <button onClick={() => setShowDelModal(true)}>Delete</button>
+                    {showDelModal && <Modal childrenHeight={220}
+                                            childrenWidth={400}
+                                            onDeleteClick={() => {
+                                                props.deleteCardsPack(props.pack._id);
+                                                setShowDelModal(false);
+                                            }}
+                                            onModalClose={() => setShowDelModal(false)}
+                                            type={'info'}
+                                            header={'Delete pack'}
+                                            buttonTitle={'Delete'}
+                                            packName={'Pack name'}/>}
+                    <button onClick={() => setShowEditModal(true)}>update</button>
+                    {showEditModal && <Modal childrenHeight={233}
+                                             childrenWidth={400}
+                                             onSaveClick={() => {
+                                                 props.updateCardsPackName(props.pack._id);
+                                                 setShowEditModal(false);
+                                             }}
+                                             onModalClose={() => setShowEditModal(false)}
+                                             type={'input'}
+                                             header={'Add new pack'}
+                                             buttonTitle={'Save'}
+                                             inputTitle={'Name pack'}/>}
                     <NavLink to={`${PATH.CARDS}/${props.pack._id}`}>cards</NavLink>
                     <NavLink to={`${PATH.LEARN}/${props.pack._id}`}>learn</NavLink>
                 </td>
