4b3ebb418680f4c6f7b24459dbe2d7c40ac876d4
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-27 13:35:59 +0300

    fix: add correct layout for page for creating new password

--- src/n2-features/f1-auth/a4-enter-new-password/EnterNewPassword.tsx
+++ src/n2-features/f1-auth/a4-enter-new-password/EnterNewPassword.tsx
@@ -1,9 +1,10 @@
-import React, {ChangeEvent, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../../n1-main/m2-bll/store";
-import {NavLink, Redirect, RouteComponentProps, withRouter} from "react-router-dom";
-import {PATH} from "../../../n1-main/m1-ui/routes/Routes";
-import {setNewPasswordTC} from "./enterNewPassword-reducer";
+import React, {ChangeEvent, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from '../../../n1-main/m2-bll/store';
+import {NavLink, Redirect, RouteComponentProps, withRouter} from 'react-router-dom';
+import {PATH} from '../../../n1-main/m1-ui/routes/Routes';
+import {setNewPasswordTC} from './enterNewPassword-reducer';
+import styles from './EnterNewPassword.module.sass';
 
 type PathParamsType = {
     token: string
@@ -12,8 +13,8 @@ type PropsType = RouteComponentProps<PathParamsType>
 
 const EnterNewPassword: React.FC<PropsType> = (props) => {
 
-    const [newPassword, setNewPassword] = useState<string>("")
-    const [correctNewPassword, setCorrectNewPassword] = useState<string>("")
+    const [newPassword, setNewPassword] = useState<string>('')
+    const [correctNewPassword, setCorrectNewPassword] = useState<string>('')
 
     const dispatch = useDispatch()
     const isNewPassword = useSelector<AppRootStateType, boolean>(state => state.newPassword.isNewPassword)
@@ -30,7 +31,7 @@ const EnterNewPassword: React.FC<PropsType> = (props) => {
         if (newPassword === correctNewPassword) {
             dispatch(setNewPasswordTC(newPassword, props.match.params.token))
         } else {
-            alert("Passwords isn't equal")
+            alert('Passwords isn\'t equal')
         }
     }
 
@@ -39,20 +40,29 @@ const EnterNewPassword: React.FC<PropsType> = (props) => {
     }
 
     return (
-
-        <div>
-            <form>
-                <input type="password" value={newPassword}
+        <div className={styles.mainWrapper}>
+            <h3>Create new password</h3>
+            <div className={styles.inputWrap}>
+                <label htmlFor="newPass/password"/>
+                <input type="password"
+                       value={newPassword}
+                       placeholder={'Password...'}
+                       id={'newPass/password'}
                        onChange={onChangeNewPasswordHandler}/>
-                <input type="password" value={correctNewPassword}
-                       onChange={onChangeCorrectNewPasswordHandler}/>
-                <div>
-                    <button type="submit" onClick={onSetNewPasswordHandler}>Send</button>
-                </div>
-            </form>
-            <div>
-                <NavLink to={PATH.LOGIN}>login</NavLink>
             </div>
+            <div className={styles.inputWrap}>
+                <label htmlFor='newPass/repeatPassword'/>
+                <input type="password"
+                       value={correctNewPassword}
+                       onChange={onChangeCorrectNewPasswordHandler}
+                       id={'newPass/repeatPassword'}
+                       placeholder={'Repeat password...'}/>
+            </div>
+            <div>Create new password and we will send you further instructions to email</div>
+            <button type="submit"
+                    onClick={onSetNewPasswordHandler}>
+                Create new password
+            </button>
         </div>
 
     )
