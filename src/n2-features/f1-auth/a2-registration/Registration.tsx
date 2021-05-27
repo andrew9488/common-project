01259115f81bf6f3ca8947062b6170bb66a4398b
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-27 11:38:03 +0300

    fix: add correct layout and 2nd input for password for reg.page

--- src/n2-features/f1-auth/a2-registration/Registration.tsx
+++ src/n2-features/f1-auth/a2-registration/Registration.tsx
@@ -1,16 +1,17 @@
-import React, {ChangeEvent, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {registerTC} from "./register-reducer";
-import {AppRootStateType} from "../../../n1-main/m2-bll/store";
-import {NavLink, Redirect} from "react-router-dom";
-import {PATH} from "../../../n1-main/m1-ui/routes/Routes";
-import style from "./Registration.module.css"
-import {RequestStatusType} from "../../../n1-main/m1-ui/app-reducer";
+import React, {ChangeEvent, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {registerTC} from './register-reducer';
+import {AppRootStateType} from '../../../n1-main/m2-bll/store';
+import {NavLink, Redirect} from 'react-router-dom';
+import {PATH} from '../../../n1-main/m1-ui/routes/Routes';
+import styles from './Registration.module.scss'
+import {RequestStatusType} from '../../../n1-main/m1-ui/app-reducer';
 
 const Registration: React.FC = () => {
 
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [checkPassword, setCheckPassword] = useState<string>('')
 
     const appStatus = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
     const isRegistration = useSelector<AppRootStateType, boolean>(state => state.register.isRegistration)
@@ -24,13 +25,18 @@ const Registration: React.FC = () => {
         setPassword(e.currentTarget.value)
     }
 
+    const onChangePasswordCheckHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheckPassword(e.currentTarget.value)
+    }
     const onRegistrationHandler = () => {
-        if (password === "" || email === "") {
-            alert("Field is required")
+        if (password === '' || email === '') {
+            alert('Field is required')
+        } else if (checkPassword !== password) {
+            alert('Passwords are different!')
         } else {
             dispatch(registerTC(email, password))
-            setEmail("")
-            setPassword("")
+            setEmail('')
+            setPassword('')
         }
     }
 
@@ -39,24 +45,41 @@ const Registration: React.FC = () => {
     }
 
     return (
-        <div className={style.registrationContainer}>
-            <h1>Registration: </h1>
-            <form className={style.formBlock}>
-                <div>
-                    <span>Email: </span><input placeholder="Enter email..." type="email" value={email}
-                                               onChange={onChangeEmailHandler}/>
-                </div>
-                <div>
-                    <span>Password: </span><input placeholder="Enter password..." type="password" value={password}
-                                                  onChange={onChangePasswordHandler}/>
-                </div>
-                <div>
-                    <button type="submit" onClick={onRegistrationHandler} disabled={appStatus === "loading"}>Register
-                    </button>
-                </div>
-            </form>
-            <div>
-                <NavLink to={PATH.LOGIN}>login</NavLink>
+        <div className={styles.registrationContainer}>
+            <h3>Sign up</h3>
+            <div className={styles.inputWrapper}>
+                <label htmlFor="'registration/email'">Email</label>
+                <input placeholder="Enter email..."
+                       type="text"
+                       value={email}
+                       onChange={onChangeEmailHandler}
+                       id={'registration/email'}/>
+            </div>
+            <div className={styles.inputWrapper}>
+                <label htmlFor="registration/password">Password</label>
+                <input placeholder="Enter password..."
+                       type="password"
+                       value={password}
+                       onChange={onChangePasswordHandler}
+                       id={'registration/password'}
+                       autoComplete={'new-password'}/>
+            </div>
+            <div className={styles.inputWrapper}>
+                <label htmlFor="registration/checkPassword">Confirm password</label>
+                <input placeholder="Confirm password..."
+                       type="password"
+                       value={checkPassword}
+                       onChange={onChangePasswordCheckHandler}
+                       id={'registration/checkPassword'}
+                       autoComplete={'new-password'}/>
+            </div>
+            <div className={styles.btnWrap}>
+                <button className={styles.cancel}>Cancel</button>
+                <button type="submit"
+                        onClick={onRegistrationHandler}
+                        disabled={appStatus === 'loading'}>
+                    Register
+                </button>
             </div>
         </div>
     )
